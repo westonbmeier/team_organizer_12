00ab94935205b3d3e33c7c38fc07dc3a4d9a7f94
westonbmeier <westonbmeier@gmail.com>
2020-03-11 16:22:38 -0600

    Set up main menu selection maker

--- app.js
+++ app.js
@@ -12,6 +12,65 @@ var con = mysql.createConnection({
   
   con.connect(function(err) {
     if (err) throw err;
+
     console.log("Connected!");
-    runManager();
+
+    selectionMaker();
   });
+
+  con.connect(function(err) {
+    if (err) throw err;
+
+    console.log("Connected!");
+
+    selectionMaker();
+  });
+
+  //Main Menu//
+  function selectionMaker() {
+      inquirer.prompt({
+          type: 'list',
+          message: 'What would you like to do?',
+          name: 'action',
+          choices: ['View departments', 'View roles', 'View employees', 'Add department', 'Add role', 'Add employee','Update a role', 'Update a department', 'Exit']
+      }).then(function(answer){
+    
+        switch (answer.action) {
+        case 'View departments':
+            viewDepartments();
+            break;
+
+        case 'View roles':
+            viewRoles();
+            break;
+
+        case 'View employees':
+            viewEmployees();
+            break;
+
+        case 'Add department':
+            addDepartment();
+            break;
+
+        case 'Add role':
+            addRole();
+            break;
+
+        case 'Add employee':
+            addEmployee();
+            break;
+
+        case 'Update a role':
+            updateRole()
+            break;
+
+        case 'Update a department':
+          updateDepartment()
+          break;
+          
+        case 'Exit':
+            con.end();
+            break;
+        }
+      });
+  }
